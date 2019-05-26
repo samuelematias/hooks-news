556eb6151daf1cbbcc6100b2604bf1fb86046a9f
Samuel Mataraso <samuelmataraso@Samuels-MacBook-Pro.local>
2019-05-26 19:17:04 -0300

    Using Async / Await for Fetching Data in useEffect

--- src/App.js
+++ src/App.js
@@ -5,13 +5,15 @@ export default function App() {
 	const [results, setResults] = useState([]);
 
 	useEffect(() => {
-		axios
-			.get('http://hn.algolia.com/api/v1/search?query=reacthooks')
-			.then(response => {
-				console.log(response.data);
-				setResults(response.data.hits);
-			});
+		getResults();
 	}, []);
+
+	const getResults = async () => {
+		const response = await axios.get(
+			'http://hn.algolia.com/api/v1/search?query=reacthooks'
+		);
+		setResults(response.data.hits);
+	};
 
 	return (
 		<Fragment>
